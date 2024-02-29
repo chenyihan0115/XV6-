94caa26333ea4a026354868aa5957d8e7a7bc502
chenyihan0115 <1186537233@qq.com>
2024-02-29 19:47:28 +0800

    lab7最终

--- notxv6/barrier.c
+++ notxv6/barrier.c
@@ -26,6 +26,16 @@ static void
 barrier()
 {
   // YOUR CODE HERE
+  pthread_mutex_lock(&bstate.barrier_mutex);
+  if(++bstate.nthread < nthread) {
+    pthread_cond_wait(&bstate.barrier_cond, &bstate.barrier_mutex);
+  } else {
+    bstate.nthread = 0;
+    bstate.round++;
+    pthread_cond_broadcast(&bstate.barrier_cond);
+  }
+  pthread_mutex_unlock(&bstate.barrier_mutex);
+
   //
   // Block until all threads have called barrier() and
   // then increment bstate.round.
--- notxv6/ph.c
+++ notxv6/ph.c
@@ -16,6 +16,8 @@ struct entry {
 struct entry *table[NBUCKET];
 int keys[NKEYS];
 int nthread = 1;
+// pthread_mutex_t lock;
+pthread_mutex_t locks[NBUCKET];
 
 double
 now()
@@ -39,6 +41,8 @@ static
 void put(int key, int value)
 {
   int i = key % NBUCKET;
+  
+  pthread_mutex_lock(&locks[i]);
 
   // is the key already present?
   struct entry *e = 0;
@@ -53,18 +57,25 @@ void put(int key, int value)
     // the new is new.
     insert(key, value, &table[i], table[i]);
   }
+
+  pthread_mutex_unlock(&locks[i]);
+
 }
 
 static struct entry*
 get(int key)
 {
   int i = key % NBUCKET;
-
+  
+  pthread_mutex_lock(&locks[i]);
 
   struct entry *e = 0;
   for (e = table[i]; e != 0; e = e->next) {
     if (e->key == key) break;
   }
+
+   
+   pthread_mutex_unlock(&locks[i]);
 
   return e;
 }
@@ -103,6 +114,8 @@ main(int argc, char *argv[])
   void *value;
   double t1, t0;
 
+  // pthread_mutex_init(&lock, NULL);
+
   if (argc < 2) {
     fprintf(stderr, "Usage: %s nthreads\n", argv[0]);
     exit(-1);
@@ -115,6 +128,9 @@ main(int argc, char *argv[])
     keys[i] = random();
   }
 
+   for(int i=0;i<NBUCKET;i++) {
+    pthread_mutex_init(&locks[i], NULL); 
+  }
   //
   // first the puts
   //
